83fbec35304999f57200cfbb505ab33cf96f2d3a
sceat <sceat@hydre.io>
2019-05-25 22:10:58 +0300

    updating tests

--- test/index.js
+++ test/index.js
@@ -1,35 +1,32 @@
-import tape from 'blue-tape'
+import tape from 'tape-promise/tape'
 import { dPromise, cache } from '../src'
 
 function delay(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms))
 }
 
-tape('A deferred promise can be resolved from outside scope', async t => {
+tape('A deferred promise', async t => {
 	const dp = new dPromise()
-	await delay(1000)
+	await delay(200)
 	dp.resolve()
-	await dp.promise
+	await t.doesNotReject(dp.promise, ...[], 'can be resolved from outside scope')
+	const dp2 = new dPromise()
+	dp2.reject()
+	await t.rejects(dp2.promise, ...[], 'can be rejected from outside scope')
 })
 
-tape('A deferred promise can be rejected from outside scope', async t => {
-	const dp = new dPromise()
-	dp.reject()
-	await t.shouldReject(dp.promise)
-})
-
-tape('A cached function should be called only once', async t => {
+tape('A cached function', async t => {
 	const foo = new Foo()
 	foo.bar()
 	foo.bar()
-	t.equal(foo.bar(), 1)
+	t.equal(foo.bar(), 1, 'should be called only once')
 })
 
-tape('A cached getter should be called only once', async t => {
+tape('A cached getter', async t => {
 	const foo = new Foo()
 	foo.baz
 	foo.baz
-	t.equal(foo.baz, 1)
+	t.equal(foo.baz, 1, 'should be called only once')
 })
 
 class Foo {
